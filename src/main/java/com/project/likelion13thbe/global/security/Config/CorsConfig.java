430678676165ebac9a422e767695c1fdd1be3477
seohyunseok <tjgustjr33@gmail.com>
2025-06-03 16:07:33 +0900

    CorsConfig에 도메인 주소 추가

--- src/main/java/com/project/likelion13thbe/global/security/Config/CorsConfig.java
+++ src/main/java/com/project/likelion13thbe/global/security/Config/CorsConfig.java
@@ -24,6 +24,7 @@ public class CorsConfig implements WebMvcConfigurer {
         allowedOriginPatterns.add("http://localhost:3000");
         allowedOriginPatterns.add("http://127.0.0.1:8080");
         allowedOriginPatterns.add("http://127.0.0.1:3000");
+        allowedOriginPatterns.add("https://hyunseok.store");
 
 
         //허용하는 HTTP METHOD 지정
